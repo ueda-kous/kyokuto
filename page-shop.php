c272cb767f4f1649553cbbce733a757965a3d3fd
ueda kous <ueda@kous.co.jp>
2021-11-22 18:17:49 +0900

    bugfix:店舗詳細

--- page-shop.php
+++ page-shop.php
@@ -25,6 +25,7 @@ if (isset($_GET['sid']) && !empty($_GET['sid'])) {
             $data = json_decode($_data['body'], true);
 
             if (isset($_GET['mode'])) {
+                define('WP_DEBUG', 1);
                 var_dump($url);
                 var_dump($_data);
                 var_dump($data);
